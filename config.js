f5fc5733f1fef91f23b136e4147027e091bf133c
pranav20395 <pranav20395@iiitd.ac.in>
2026-02-26 03:02:40 +1100

    Configure Supabase project keys

---
+++ config.js
@@ -0,0 +1,5 @@
+window.APP_CONFIG = {
+  SUPABASE_URL: "https://xettdqvkbxxqqdpqorhq.supabase.co",
+  SUPABASE_ANON_KEY:
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhldHRkcXZrYnh4cXFkcHFvcmhxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIwMjgwMTcsImV4cCI6MjA4NzYwNDAxN30.gKv3EDKoqvZkhDYMZZa2AvUsuycSsi_aVnR_i7Ho6pc"
+};
